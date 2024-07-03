13a3f0ad2e0edc3c4b34ccfeaa7d9c17e10db2ad
ScottCPetty <scottishcheese13@gmail.com>
2024-07-03 12:13:41 -0400

    function boilerplate

--- server.js
+++ server.js
@@ -1,4 +1,19 @@
 const express = require("express");
-const app = express()
+const app = express();
 const pg = require("pg");
-const client = new pg.Client(process.env.DATABASE_URL || "postgres://localhost/acme_hr_directory")
+const client = new pg.Client(
+  process.env.DATABASE_URL || "postgres://localhost/acme_hr_directory"
+);
+const port = process.env.PORT || 3000;
+
+async function init() {
+  await client.connect();
+  const SQL = ``;
+  await client.query(SQL);
+  console.log("tables created");
+  SQL = ``;
+  await client.query(SQL);
+  console.log("tables seeded");
+  app.listen(port, () => console.log(`listening on port ${port}`));
+}
+init();
